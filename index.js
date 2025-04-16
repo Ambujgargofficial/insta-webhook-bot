38669dd1ed5a717d601abbecfa404c00f9776e9e
Ambujgargofficial <smsales@shrimasha.com>
2025-04-16 15:39:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Webhook running on port ${PORT}`));
